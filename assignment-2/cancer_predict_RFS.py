ebd91a3b42b89e51d57392c878cdc75c89d3aefa
Ayden <weideng@aliyun.com>
2024-11-14 23:34:12 +0000

    * 初始化作业2关于：data progressing以及feature selection, and ML Method Development的代码

--- assignment-2/cancer_predict_RFS.py
+++ assignment-2/cancer_predict_RFS.py
@@ -17,6 +17,11 @@ from sklearn.feature_selection import SelectKBest, f_classif
 
 from scipy import stats
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+
 # Data processing模块
 print("Data processing模块从这里开始")  # 正式上线时候删除
 file_path = 'traning_data/TrainDataset.xls'
@@ -65,7 +70,18 @@ print("Selected Features:\n", selected_features)
 print("Preprocessed Data with Selected Features:\n", data_selected.head())
 
 
+# ML Method Development模块
+X_train, X_test, y_train, y_test = train_test_split(data_selected.drop(columns=['Gene']), data_selected['Gene'], test_size=0.2, random_state=42)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
+accuracy = accuracy_score(y_test, y_pred)
+confusion_mat = confusion_matrix(y_test, y_pred)
+classification_rep = classification_report(y_test, y_pred)
+print(accuracy)
+print(confusion_mat)
+print(classification_rep)
 
 
 
